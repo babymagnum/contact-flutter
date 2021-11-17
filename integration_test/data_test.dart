23f4520212d304dc51ceca21ec4679b6539c799f
ariefzainuri <info@dihardjasoftware.com>
2021-11-17 14:25:24 +0700

    Update unit test

--- integration_test/data_test.dart
+++ integration_test/data_test.dart
@@ -7,17 +7,22 @@
 
 import 'package:get/get.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:skor_id_flutter/main.dart';
 import 'package:skor_id_flutter/model/app_config.dart';
+import 'package:skor_id_flutter/model/contact_list.dart';
 import 'package:skor_id_flutter/networking/service/http_service.dart';
 import 'package:skor_id_flutter/utils/controller/common_controller.dart';
 import 'package:skor_id_flutter/utils/helper/constant.dart';
+import 'package:skor_id_flutter/view/base_view.dart';
+import 'package:skor_id_flutter/view/contact/contact_view.dart';
+import 'package:skor_id_flutter/view/contact/widget/contact_list_item.dart';
 
 void main() {
+  var commonCt = Get.put(CommonController());
+
+  commonCt.appConfig(AppConfig(Constant.API_PRODUCTION, '', true));
+
   test('Http request test', () async {
-    var commonCt = Get.put(CommonController());
-
-    commonCt.appConfig(AppConfig(Constant.API_PRODUCTION, '', true));
-
     final response = await HttpService().getContact();
 
     expect(response.map((e) => e.toJson()).toList(), equals(Constant.exampleData));
