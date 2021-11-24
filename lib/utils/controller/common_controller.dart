6c889620c74bdbd161ed8546112c70bf1ba3e4f7
arief_dihardja <arief@dihardjasoftware.com>
2021-11-24 20:42:00 +0700

    Remove unused codes

--- lib/utils/controller/common_controller.dart
+++ lib/utils/controller/common_controller.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:skor_id_flutter/model/app_config.dart';
